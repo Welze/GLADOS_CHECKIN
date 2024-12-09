7e6a4209c6cbbb404b30bd3c40cee22c2dbbb16b
zouwei <we_zou@eaglenice.cn>
2024-12-09 08:42:18 +0800

    update PushPlus URL

--- glados.py
+++ glados.py
@@ -1,15 +1,14 @@
 import datetime
 
-import requests,os,json
-
+import requests, os, json
 
 if __name__ == '__main__':
-#   pushplus token
-    pkey = os.environ.get('PUSHPLUS_TOKEN','')
-#   消息推送
+    #   pushplus token
+    pkey = os.environ.get('PUSHPLUS_TOKEN', '')
+    #   消息推送
     sendmsg = ''
-#   GLaDOS cookie
-    cookies = os.environ.get('GLADOS_COOKIES',[]).split('&')
+    #   GLaDOS cookie
+    cookies = os.environ.get('GLADOS_COOKIES', []).split('&')
     if cookies[0] == '':
         print('未获取到GLADOS_COOKIES环境变量')
         cookies = []
@@ -24,9 +23,12 @@ if __name__ == '__main__':
     }
     for cookie in cookies:
         checkin = requests.post(checkin_url,
-                                headers={'cookie':cookie,'referrer':referrer,'origin':origin,'user-agent':useragent,'content-type':'application/json; charset=utf-8'},data = json.dumps(payload))
-        status = requests.get(status_url,headers={'cookie':cookie,'referrer':referrer,'origin':origin,'useragent':useragent})
-        
+                                headers={'cookie': cookie, 'referrer': referrer, 'origin': origin,
+                                         'user-agent': useragent, 'content-type': 'application/json; charset=utf-8'},
+                                data=json.dumps(payload))
+        status = requests.get(status_url, headers={'cookie': cookie, 'referrer': referrer, 'origin': origin,
+                                                   'useragent': useragent})
+
         days = str(status.json()['data']['leftDays']).split('.')[0]
         email = status.json()['data']['email']
 
@@ -35,18 +37,16 @@ if __name__ == '__main__':
 
         if 'message' in checkin.text:
             msg = checkin.json()['message']
-            print(email+'|'+'剩余：'+days+'天|'+msg+'|积分：'+balance+'|变化：'+ change +'\n')
-            sendmsg += email+'|'+'剩余：'+days+'天|'+msg+'|积分：'+balance+'|变化：'+ change +'\n'
+            if 'Checkin Repeats' in msg:
+                sendmsg += '签到失败，重复签到！'
+                # print(sendmsg)
+            elif 'Checkin! Got' in msg:
+                # print(email + '剩余：' + days + '天|' + msg + '|积分：' + balance + '|变化：' + change + '\n')
+                sendmsg += email + '剩余：' + days + '天|' + msg + '|积分：' + balance + '|变化：+' + change + '\n'
         else:
             sendmsg += email + '签到失败，请更新cookies'
 
-    if pkey !='':
-        requests.get( 'http://www.pushplus.plus/send?token=' + pkey + '&title=GLaDOS签到情况&content=' + sendmsg)
-
-
-
-
-
-
-
-
+    if pkey != '':
+        sendUrl = 'http://pushplus.hxtrip.com/send?token=' + pkey + '&title=GLaDOS签到情况&content=' + sendmsg
+        # print(sendUrl)
+        requests.get(sendUrl)
